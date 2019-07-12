29d79cd378d9778def648c229feb0e5fa0d91127
fbadilla <f.badilla228@gmail.com>
2019-07-12 14:41:50 +0000

    modelo insta

--- src/models.py
+++ src/models.py
@@ -8,23 +8,46 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class Users(Base):
+    __tablename__ = 'Users'
+    # Here we define columns for python3 src/models.pythe table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(15), nullable=False)
+    user = Column(String(10), nullable=False)
+    sitio_web = Column(String(30), nullable=False)
+    email = Column(String(20), nullable=False)
+    phone = Column(String(12), )
 
-class Address(Base):
-    __tablename__ = 'address'
+class Posts(Base):
+    __tablename__ = 'posts'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    description = Column(String(250))
+    date = Column(String(250))
+    users_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(Users)
+
+class Coments(Base):
+    __tablename__ = 'coments'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    date = Column(String(250))
+    users_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(Users)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user_post = relationship(Posts)
+class Follows(Base):
+    __tablename__ = 'follows'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    date = Column(String(250))
+    userW = relationship(Users)
+    userF = relationship(Users)
 
     def to_dict(self):
         return {}
